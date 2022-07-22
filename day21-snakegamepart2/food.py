9e2171368bd87d93fc37f798652c06fbb45c4712
unchartedsoul <56485884+unchartedsoul@users.noreply.github.com>
2022-07-22 17:05:38 +0530

    day21
    😁

---
+++ day21-snakegamepart2/food.py
@@ -0,0 +1,25 @@
+import random
+from turtle import Turtle
+
+class Food(Turtle):
+
+    def __init__(self):
+        super().__init__()
+        self.shape("circle")
+        self.penup()
+        self.shapesize(stretch_wid=0.5, stretch_len=0.5)
+        self.color("blue")
+        self.speed("fastest")
+        random_x = random.randint(-280, 280)
+        random_y = random.randint(-280, 280)
+        self.goto(random_x, random_x)
+        self.refesh()
+
+
+    def refesh(self):
+        random_x = random.randint(-280, 280)
+        random_y = random.randint(-280, 280)
+        self.goto(random_x, random_x)
+
+
+
